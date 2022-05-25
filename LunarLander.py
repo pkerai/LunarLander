75bee40f9ceaa01ce9f4b6c8b66308db47b9802a
pkerai <70039224+pkerai@users.noreply.github.com>
2022-05-25 09:36:20 +0100

    first commit

---
+++ LunarLander.py
@@ -0,0 +1,12 @@
+#Lunar Lander project
+# This program is a game which is about trying to land a space ship on the moom.
+# The program aks the user how much of the fuel it should burn to decellerate
+# the space ship. The idea of the program is to land on the moon without crashing.
+# This is achieved if the landing velocity is less than 10 metres/second.
+import sys
+h = 1000  # height in metres
+vel = 0  # velocity in m/s
+f = 1000  # fuel in litres
+fl = 0
+t = 0   #time at start
+fuelleft=0
